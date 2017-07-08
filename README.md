7d98dadd8072ddf66c03a17183de6f6493638152
Pascal Boittin <pascalboittin@live.nl>
2017-07-08 17:05:27 +0200

    basis chessboard

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # js
 javascript experiments
 http://htmlpreview.github.io/?https://github.com/Odusseus/js/blob/master/chessboard/cb.html
+
+https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces
--- chessboard/js/cb.js
+++ chessboard/js/cb.js
@@ -2,6 +2,7 @@ var Ocb = {}
 
 Ocb.boardWidth = 400;
 Ocb.squareWidth = 50;
+Ocb.pieceWidth = 48;
 
     Ocb.resizeBoard = function(x){
 
@@ -12,24 +13,37 @@ Ocb.squareWidth = 50;
 
     Ocb.resizeSquare = function(x){
         var squares = document.getElementsByClassName("square");
+
         for(var i=0; i<squares.length; i++){
             squares[i].setAttribute("style","width:" + x.toString() + "px; height:" + x.toString() + "px");
         }
     };
 
+    Ocb.resizePiece = function(x){
+        var squares = document.getElementsByClassName("piece");
+
+        for(var i=0; i<squares.length; i++){
+            squares[i].setAttribute("style","width:" + x.toString() + "px; height:" + x.toString() + "px;");
+        }
+    };
+
     Ocb.zoomIn = function(){
-        Ocb.boardWidth -= 10;
+        Ocb.boardWidth -= 20;
         Ocb.squareWidth = Ocb.boardWidth / 8;
+        Ocb.pieceWidth = Ocb.squareWidth - (Ocb.squareWidth * 0.1);
 
         Ocb.resizeSquare(Ocb.squareWidth);
+        Ocb.resizePiece(Ocb.pieceWidth);
         Ocb.resizeBoard(Ocb.boardWidth);
     };
 
     Ocb.zoomOut = function(){
-        Ocb.boardWidth += 10;
+        Ocb.boardWidth += 20;
         Ocb.squareWidth = Ocb.boardWidth / 8;
+        Ocb.pieceWidth = Ocb.squareWidth - (Ocb.squareWidth * 0.1);
 
         Ocb.resizeSquare(Ocb.squareWidth);
+        Ocb.resizePiece(Ocb.pieceWidth);
         Ocb.resizeBoard(Ocb.boardWidth);
     };
 
