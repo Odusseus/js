1f10e3f7a24fc2de0bde9cae54dbdc3e1ab5a983
Pascal Boittin <pascalboittin@live.nl>
2017-07-08 15:25:19 +0200

    Basis squares

--- chessboard/js/cb.js
+++ chessboard/js/cb.js
@@ -1,6 +1,7 @@
 var Ocb = {}
 
 Ocb.boardWidth = 400;
+Ocb.squareWidth = 50;
 
     Ocb.resizeBoard = function(x){
 
@@ -9,13 +10,26 @@ Ocb.boardWidth = 400;
 
     };
 
+    Ocb.resizeSquare = function(x){
+        var squares = document.getElementsByClassName("square");
+        for(var i=0; i<squares.length; i++){
+            squares[i].setAttribute("style","width:" + x.toString() + "px; height:" + x.toString() + "px");
+        }
+    };
+
     Ocb.zoomIn = function(){
         Ocb.boardWidth -= 10;
+        Ocb.squareWidth = Ocb.boardWidth / 8;
+
+        Ocb.resizeSquare(Ocb.squareWidth);
         Ocb.resizeBoard(Ocb.boardWidth);
     };
 
     Ocb.zoomOut = function(){
         Ocb.boardWidth += 10;
+        Ocb.squareWidth = Ocb.boardWidth / 8;
+
+        Ocb.resizeSquare(Ocb.squareWidth);
         Ocb.resizeBoard(Ocb.boardWidth);
     };
 
