bbe2f7a1d1ff3ff5a74b0e93d7f8efa2916330a6
Pascal Boittin <pboittin@onvz.nl>
2017-09-09 01:08:04 +0200

    save

--- svsg/js/svsg.js
+++ svsg/js/svsg.js
@@ -1,27 +1,54 @@
 var Svsg = {};
 
+Svsg.Globals = function(){
+    this.size = 0;
+};
+
 Svsg.SetOutput = function(outputFieldname, value) {
     
     var outputField = document.getElementById(outputFieldname);
     outputField.innerHTML = value;
 };
 
+Svsg.GetColumn = function(){
+    var column = 0;
+
+    column = Math.floor(Math.random() * Svsg.Globals.size);
+
+    return column;
+};
+
 Svsg.Queen = function(id) {
     this.id = id;
+    this.line = this.id;
+    this.GetColumn = function(){
+        var column = 0;        
+        column = Math.floor(Math.random() * Svsg.Globals.size);
+        return column;
+    };
+    this.GetReach = function(){
+       
+        return 0;
+    };
+    this.column = this.GetColumn();
+    this.reach = this.GetReach;
 };
 
 Svsg.Go = function(size, outputFieldname) {
-    var fields = size * size;
+
+    Svsg.Globals.size = size;
+
+    var fields = Svsg.Globals.size * Svsg.Globals.size;
 
     var queens = [];
     
-    for(i = 0; i < size; i++){
+    for(i = 0; i < Svsg.Globals.size; i++){
         queens.push(new Svsg.Queen(i));
     }
     
     var queensMessage = "";
     queens.forEach(function(element) {
-      queensMessage += element.id + ",";
+      queensMessage += "(" + element.id + "," +element.line +","+ element.column + "),";
   }, this); 
 
    var output = fields + " : " + queensMessage;
