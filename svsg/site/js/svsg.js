96a6ab2532819e1d22185cb80b585ffb17cbf0cc
Pascal Boittin <pboittin@onvz.nl>
2017-09-09 17:52:40 +0200

    unittest direction

--- svsg/site/js/svsg.js
+++ svsg/site/js/svsg.js
@@ -1,53 +1,84 @@
 var Svsg = {};
 
-Svsg.Globals = function(){
+Svsg.globals = function(){
     this.size = 0;
 };
 
-Svsg.SetOutput = function(outputFieldname, value) {
+Svsg.queenDirectionEnum = {
+    UP :        {value: 0, name: "Up", code: "UP"},
+    UPRIGHT :   {value: 1, name: "Up-Right", code: "UR"},
+    RIGHT :     {value: 2, name: "Right", code: "RI"},
+    DOWNRIGHT : {value: 3, name: "Down-Right", code: "DR"},
+    DOWN :      {value: 4, name: "Down", code: "DO"},
+    DOWNLEFT :  {value: 5, name: "Down-Left", code: "DL"},
+    LEFT :      {value: 6, name: "Left", code: "LE"},
+    UPLEFT :    {value: 7, name: "Up-Left", code: "UL"},
+};
+
+Svsg.direction = function(directionEnum) {
+    this.current = 0;
+    this.directionEnum = directionEnum;
+    this.setCurrentDirection = function(){
+        for(var key in this.directionEnum){
+            var element = this.directionEnum[key];
+            if(element.value == this.current){                
+                return element;
+            }
+        }
+        return undefined;
+    };
+    
+    this.next = function() {
+        this.current++;
+        this.currentDirection = this.setCurrentDirection();
+    };
+    this.currentDirection = this.setCurrentDirection();
+};
+
+Svsg.setOutput = function(outputFieldname, value) {
     
     var outputField = document.getElementById(outputFieldname);
     outputField.innerHTML = value;
 };
 
-Svsg.GetColumn = function(){
-    var column = 0;
-
-    if(!Svsg.Globals.size) {
-        Svsg.Globals.size = 0;
-    }
-
-    column = Math.floor(Math.random() * Svsg.Globals.size);
-
-    return column;
-};
-
-Svsg.Queen = function(id) {
+Svsg.queen = function(id) {
     this.id = id;
     this.line = this.id;
-    this.GetColumn = function(){
+    this.getColumn = function(){
         var column = 0;        
-        column = Math.floor(Math.random() * Svsg.Globals.size);
+        if(!Svsg.globals.size){
+            Svsg.globals.size = 0;
+        }
+        column = Math.floor(Math.random() * Svsg.globals.size);
         return column;
     };
+    this.column = this.getColumn();
 
-    this.GetReach = function(){       
+    this.getReach = function(){ 
+        
+        // for(var currentDirection = Svsg.QueenDirectionEnum.UP;
+        //     currentDirection.value < Svsg.QueenDirectionEnum.MAX; 
+        //     ){
+
+        // }
+
+        var directions = [""];
+
         return 0;
     };
-    this.column = this.GetColumn();
-    this.reach = this.GetReach();
+    this.reach = this.getReach();
 };
 
-Svsg.Go = function(size, outputFieldname) {
+Svsg.go = function(size, outputFieldname) {
 
-    Svsg.Globals.size = size;
+    Svsg.globals.size = size;
 
-    var fields = Svsg.Globals.size * Svsg.Globals.size;
+    var fields = Svsg.globals.size * Svsg.globals.size;
 
     var queens = [];
     
-    for(i = 0; i < Svsg.Globals.size; i++){
-        queens.push(new Svsg.Queen(i));
+    for(i = 0; i < Svsg.globals.size; i++){
+        queens.push(new Svsg.queen(i));
     }
     
     var queensMessage = "";
@@ -57,7 +88,7 @@ Svsg.Go = function(size, outputFieldname) {
 
    var output = fields + " : " + queensMessage;
 
-    Svsg.SetOutput(outputFieldname, output);
+    Svsg.setOutput(outputFieldname, output);
 };
 
 
--- svsg/test/tests.js
+++ svsg/test/tests.js
@@ -1,14 +1,31 @@
-QUnit.test( "Default Svsg.GetColumn() return 0", function( assert ) {
+QUnit.test( "Default queen.getColumn() return 0", function( assert ) {
 
-    var column = Svsg.GetColumn();
+    var queen = new Svsg.queen(0);
+    var column = queen.getColumn();
 
     assert.ok( column == 0, "Passed!" );
   });
 
-  QUnit.test( "Svsg.GetColumn() and Size is 10 return >= 0 <= 10", function( assert ) {
+  QUnit.test( "queen.getColumn() and Size is 10 return >= 0 <= 10", function( assert ) {
     
-        Svsg.Globals.size = 10;
-        var column = Svsg.GetColumn();
+        Svsg.globals.size = 10;
+        var queen = new Svsg.queen(0);
+        var column = queen.getColumn();
     
         assert.ok( column >= 0 && column <= 10, "Passed!" );
+      });
+
+QUnit.test( "defaut direction", function( assert ) {
+  
+      var result = new Svsg.direction(Svsg.queenDirectionEnum);
+        
+      assert.ok( result.currentDirection == Svsg.queenDirectionEnum.UP, "Passed!" );
+    });
+
+QUnit.test( "next direction", function( assert ) {
+    
+        var result = new Svsg.direction(Svsg.queenDirectionEnum);
+        result.next();
+          
+        assert.ok( result.currentDirection == Svsg.queenDirectionEnum.UPRIGHT, "Passed!" );
       });
