2c7c7ec9d4d7700f972bb5176e69be28ce3ddd44
Pascal Boittin <pboittin@onvz.nl>
2017-09-10 02:24:56 +0200

    Backup

--- svsg/site/js/svsg.js
+++ svsg/site/js/svsg.js
@@ -1,17 +1,34 @@
 var Svsg = {};
 
+Svsg.formatNumberLength = function(num, length) {
+    var r = "" + num;
+    while (r.length < length) {
+        r = "0" + r;
+    }
+    return r;
+};
+
 Svsg.globals = function(){
     this.size = 1;
+};
+Svsg.getSizeLenght = function(){
+            var string = "" + Svsg.globals.size;
+            return string.length;
+        };
+
+Svsg.ColumnLineToId = function(column, line){
+    if(!Svsg.globals.size){
+        Svsg.globals.size = 1;
+    }
+    var id = Svsg.globals.size * (line - 1) + column; 
+    return id;
 };
 
 Svsg.field = function(column, line){
     this.column = column;
     this.line = line;
-
-    if(!Svsg.globals.size){
-        Svsg.globals.size = 1;
-    }
-    this.id = Svsg.globals.size * (line - 1) + column; 
+  
+    this.id = Svsg.ColumnLineToId(column, line); 
 };
 
 Svsg.queenDirectionEnum = {
@@ -76,12 +93,14 @@ Svsg.queen = function(id) {
         return fields;
     };
 
-    this.getDownFields = function(){
+    this.getUpRightFields = function(){
         var fields = [];
+        var column = this.column;
         var line = this.line;
-        while(line > 1) {
-            line--;
-            var field = new Svsg.field(this.column, line);
+        while(colimn < Svsg.globals.size && line < Svsg.globals.size) {
+            column++;
+            line++;
+            var field = new Svsg.field(column, line);
             fields.push(field);
         }
         return fields;
@@ -98,6 +117,43 @@ Svsg.queen = function(id) {
         return fields;
     };
 
+    this.getDownRightFields = function(){
+        var fields = [];
+        var column = this.column;
+        var line = this.line;
+        while(column < Svsg.globals.size && line > 1) {
+            column ++;
+            line--;
+            var field = new Svsg.field(column, line);
+            fields.push(field);
+        }
+        return fields;
+    };
+
+    this.getDownFields = function(){
+        var fields = [];
+        var line = this.line;
+        while(line > 1) {
+            line--;
+            var field = new Svsg.field(this.column, line);
+            fields.push(field);
+        }
+        return fields;
+    };
+
+    this.getLeftDownFields = function(){
+        var fields = [];
+        var column = this.column;
+        var line = this.line;
+        while(column > 1 && line > 1) {
+            column--;
+            line--;
+            var field = new Svsg.field(column, line);
+            fields.push(field);
+        }
+        return fields;
+    };
+
     this.getLeftFields = function(){
         var fields = [];
         var column = this.column;
@@ -109,7 +165,22 @@ Svsg.queen = function(id) {
         return fields;
     };
 
-    this.getReach = function(){ 
+    this.getLeftUpFields = function(){
+        var fields = [];
+        var column = this.column;
+        var line = this.line;
+        while(column > 1 && line < Svsg.globals.size) {
+            column--;
+            line++;
+            var field = new Svsg.field(column, line);
+            fields.push(field);
+        }
+        return fields;
+    };
+
+    this.reach = [];
+
+    this.setReach = function(){ 
         var fields = [];
         new Svsg.field(this.column, this.line);
 
@@ -117,22 +188,64 @@ Svsg.queen = function(id) {
              currentDirection.value < Object.keys(Svsg.queenDirectionEnum).length;
              currentDirection.next()
              ){
-                 if(currentDirection == Svsg.queenDirectionEnum.UP) {
+                if(currentDirection == Svsg.queenDirectionEnum.UP) {
                     fields.concat(this.getUpFields());
                 }
-                if(currentDirection == Svsg.queenDirectionEnum.DOWN) {
-                    fields.concat(this.getDownFields());
-               }
+                if(currentDirection == Svsg.queenDirectionEnum.UPRIGHT) {
+                    fields.concat(this.getUpRightFields());
+                }
                 if(currentDirection == Svsg.queenDirectionEnum.RIGHT) {
                     fields.concat(this.getRightFields());
                 }
+                if(currentDirection == Svsg.queenDirectionEnum.DOWNRIGHT) {
+                    fields.concat(this.getDownRightFields());
+                }
+                if(currentDirection == Svsg.queenDirectionEnum.DOWN) {
+                    fields.concat(this.getDownFields());
+                }
+                if(currentDirection == Svsg.queenDirectionEnum.DOWNLEFT) {
+                    fields.concat(this.getDownLeftFields());
+                }
                 if(currentDirection == Svsg.queenDirectionEnum.LEFT) {
                     fields.concat(this.getLeftFields());
+                }
+                if(currentDirection == Svsg.queenDirectionEnum.UPLEFT) {
+                    fields.concat(this.getUpLeftFields());
                 }                
          }        
-        return fields;
+         this.reach = fields;
     };
-    this.reach = this.getReach();
+    
+    this.getFieldIds = function(){
+        var ids = [];
+        this.reach.forEach(function(element) {
+            ids.push(element.id);            
+        }, this);
+        ids.sort();
+    };
+    this.fieldIds = this.getFieldIds();
+    this.displayFields = function(){
+        var output = "";
+        var repeat = Svsg.globals.size * Svsg.getSizeLenght();
+        output += "<div>"; 
+        for(var line = Svsg.globals.size; line > 1; line--){
+            output += '_'.repeat(repeat); 
+            output += Svsg.formatNumberLength(line, Svsg.getSizeLenght())  + "|";
+            for(var column = 1; column < Svsg.globals.size; column++){
+                var id = Svsg.ColumnLineToId(column, line);
+              if(this.fieldIds[id] != undefined){
+                output += Svsg.formatNumberLength(1, Svsg.getSizeLenght());
+            }
+            else {
+                output += '.'.repeat(repeat); 
+              }
+            }
+            output += "<br>"; 
+            output += '_'.repeat(repeat); 
+        }
+        output += "</div>"; 
+        return output;
+    };
 };
 
 Svsg.go = function(size, outputFieldname) {
@@ -144,6 +257,9 @@ Svsg.go = function(size, outputFieldname) {
     var queens = [];
     
     for(i = 0; i < Svsg.globals.size; i++){
+        var queen = new Svsg.queen(i);
+        queen.setReach();
+
         queens.push(new Svsg.queen(i));
     }
     
@@ -152,8 +268,11 @@ Svsg.go = function(size, outputFieldname) {
       queensMessage += "(" + element.id + "," +element.line +","+ element.column + "),";
   }, this); 
 
-   var output = fields + " : " + queensMessage;
+   //var output = fields + " : " + queensMessage;
 
+    //Svsg.setOutput(outputFieldname, output);
+
+    var output = queens[0].displayFields();
     Svsg.setOutput(outputFieldname, output);
 };
 
