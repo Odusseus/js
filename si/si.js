eb0a790997fc31e2df46822e42ef80697ba0126a
Pascal Boittin <pascalboittin@live.nl>
2018-01-03 22:13:24 +0100

    version update

--- si/si.js
+++ si/si.js
@@ -12,7 +12,7 @@ si.Constant = {
     RIGHTBOARD : 320,
     TOPBOARD : 30,
     FLOORBOARD : 370,
-    VERSION : "1.0.38",
+    VERSION : "1.0.39",
     DEFAULTTYPE : 0,
     DEFENDER : 1,
     INVADERTYPE : 2,
