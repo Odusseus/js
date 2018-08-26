8800b72759541cc09658fccb6fd9e9f592eabf87
Pascal Boittin <pascalboittin@live.nl>
2018-08-26 16:46:03 +0200

    test

--- tools/js/tools.js
+++ tools/js/tools.js
@@ -61,6 +61,7 @@ function GoListOfDays(startDateName, endDateName, onlyDayId, endTextName) {
     if(endDate < startDate){
         output("Nothing to show : end date < start date.");
         return;
+        
     }
 
     var listOfDays = "";
