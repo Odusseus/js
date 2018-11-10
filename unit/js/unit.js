08525ea87220730e51fbf1b0983320e7e97e3c1e
Pascal Boittin <pboittin@onvz.nl>
2018-11-10 19:02:25 +0100

    stap 2

--- unit/js/unit.js
+++ unit/js/unit.js
@@ -1,14 +1,22 @@
+
 var Unit = {
     unit1: new Vue({
         el: '#unit-1',
         data: {
             name: 'One',
+            isActive: false,
             stylePosition: {
                 position: 'absolute',
                 top: '400px',
                 left: '500px'
-                
+            }
+        },
+        methods: {
+            select: function (event) {
+                this.isActive = !this.isActive;
+
             }
         }
     })
 };
+
---
+++ unit/readme.md
@@ -0,0 +1,5 @@
+# Memo
+
+<https://www.w3schools.com/tags/tryit.asp?ilename=tryhtml5_ev_ondrag>
+
+<https://vuejs.org/v2/guide/single-file-components.html>
