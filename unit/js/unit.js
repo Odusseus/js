ac199724001f339e7955c7fb37cc33a013958810
Pascal Boittin <pboittin@onvz.nl>
2018-11-10 17:40:36 +0100

    basis version

---
+++ unit/js/unit.js
@@ -0,0 +1,13 @@
+var Unit = {
+    unit1: new Vue({
+        el: '#unit-1',
+        data: {
+            name: 'One',
+            stylePosition: {
+                position: 'absolute',
+                top: '400px',
+                left: '500px'
+            }
+        }
+    })
+};
