d0c575a2af112d26a61a4fce1bbc9086206bab50
odusseus <pascalboittin@live.nl>
2019-08-25 23:37:27 +0200

    update write version file

--- key/ps/version.ps1
+++ key/ps/version.ps1
@@ -2,6 +2,9 @@ $version = Get-Content .\version.txt
 [int]$versionNr = [convert]::ToInt32($version, 10)
 $versionNr++
 Write-Output $versionNr
-$versionNr > .\version.txt
+
+Set-Content -Path .\version.txt -Encoding UTF8 -Value $versionNr.ToString()
+
 $variable = "const VERSION = " + $versionNr +";"
-$variable > ..\\version.js
+Set-Content -Path ..\\version.js -Encoding UTF8 -Value $variable
+
