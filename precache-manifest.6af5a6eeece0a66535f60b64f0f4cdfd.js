f8bbcd38e64e9a897619384b24818821a893555a
odusseus <info@odusseus.org>
2019-12-28 18:47:33 +0100

    Updates

--- precache-manifest.6af5a6eeece0a66535f60b64f0f4cdfd.js
+++ precache-manifest.6af5a6eeece0a66535f60b64f0f4cdfd.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd7a463c5f51171d54157259245f2bb3",
+    "revision": "56d38bbd022efdeffec86dda7a2fdd15",
     "url": "/js/index.html"
   },
   {
-    "revision": "3403cc5603a3e9e1ddcb",
-    "url": "/js/static/css/main.a8e88bab.chunk.css"
+    "revision": "bc1fb2c95a504e0fbea1",
+    "url": "/js/static/css/main.0a745c00.chunk.css"
   },
   {
     "revision": "a81873eca96009b8a945",
     "url": "/js/static/js/2.ab7a2738.chunk.js"
   },
   {
-    "revision": "3403cc5603a3e9e1ddcb",
-    "url": "/js/static/js/main.f70a08b5.chunk.js"
+    "revision": "bc1fb2c95a504e0fbea1",
+    "url": "/js/static/js/main.d3381a97.chunk.js"
   },
   {
     "revision": "0679b577e7053dc10b19",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/js/precache-manifest.ade3f88cc2086eb54a97fd25fd4be8d0.js"
+  "/js/precache-manifest.6af5a6eeece0a66535f60b64f0f4cdfd.js"
 );
 
 self.addEventListener('message', (event) => {
