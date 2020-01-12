2a57c591e2fe0e313e7350dd0ccbdbaf2ef78920
odusseus <info@odusseus.org>
2020-01-12 12:38:34 +0100

    Refactoring Class components to Functional component.

--- reactjs/chess-season/src/ShowEventsFC.js
+++ reactjs/chess-season/src/ShowEventsFC.js
@@ -1,9 +1,9 @@
 /*jshint esversion: 6 */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Moment from 'react-moment';
 import EventDisplay from './EventDisplay';
 import Event from './Event';
-// import PostEvent from './PostEvent';
+import PostEvent from './PostEvent';
 import styles from './css/cs.module.css';
 import Keys from './Keys';
 
@@ -70,9 +70,6 @@ const ShowEventsFC = () =>  {
     setEvents(updatedEvents);
   }
 
-  const onSave = () =>{
-  }
-
   const toggleShowDebug = () => {
     let newIsShowDebug = isShowDebug ? false : true;
     setIsShowDebug(newIsShowDebug);
@@ -94,6 +91,91 @@ const ShowEventsFC = () =>  {
     let newIsShowNewEvent = isShowNewEvent ? false : true;
     setIsShowNewEvent(newIsShowNewEvent);
   }
+
+  const onSave = () => {
+    let site = window.location.href;
+    const retrieveKeys = localStorage.getItem('keys');
+    let keys = JSON.parse(retrieveKeys);
+    if(!keys){
+      keys = new Keys();
+      if (site.includes("localhost")){
+        keys.key = "4265AC3D-DD4B-427C-8BFD-6D7E7BB92C09";
+        keys.token = "testToken";
+      }
+    }
+    
+    let urlBase = "https://www.odusseus.org/php/item";
+    // Site run localy and you will use the local Api.
+    const isApiLocal = true;
+    if (site.includes("localhost") && isApiLocal){
+      urlBase = "http://localhost:9000";
+    }
+    
+    let url = urlBase + "/postitem.php";
+    let value = JSON.stringify(events);
+    let postEvent = new PostEvent(keys.key, keys.token, value);
+
+    let myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+    myHeaders.append('Content-Type', 'application/json');
+
+  fetch(url, {
+      method: 'POST', 
+      body: JSON.stringify(postEvent)
+      }
+    ).then(res => res.json())
+    .then((data)=> {
+      let keys = new Keys(data.key, data.token);
+      localStorage.setItem('keys', JSON.stringify(keys));
+      setKey(keys.key);
+      setToken(keys.token);
+    })
+    .catch(console.log);
+}
+
+  useEffect(() => {
+    const retrieveKeys = localStorage.getItem('keys');
+    let keys = JSON.parse(retrieveKeys);
+    if(!keys){
+      keys = new Keys();
+      if (site.includes("localhost")){
+        keys.key = "4265AC3D-DD4B-427C-8BFD-6D7E7BB92C09";
+        keys.token = "testToken";
+      }
+    }
+    setKey(keys.key);
+    setToken(keys.token);
+    
+    let urlBase = "https://www.odusseus.org/php/item";
+    if (site.includes("localhost")){
+      urlBase = "http://localhost:9000";
+    }
+    let url = urlBase + "/getitem.php?key=" + keys.key + "&token=" + keys.token;
+    
+    setSource(urlBase);
+    fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+        let values = JSON.parse(data.value);
+        let newEvents = [];
+        let today = new Date();
+        today.setHours(0,0,0,0);
+        values.forEach(element => {
+          if(new Date(element.date) >= today){
+            let eventDisplay = new EventDisplay(element);
+            newEvents.push(eventDisplay);
+          }            
+        });
+          newEvents.sort(
+            (a, b) => 
+            ( 
+              new Date(a.date) - new Date(b.date)
+              ));
+
+          setEvents(newEvents);
+    })
+    .catch(console.log);
+  }, []); 
 
   return (
       <>
@@ -145,25 +227,25 @@ const ShowEventsFC = () =>  {
                 <label className={styles.inputFieldLabel}> 
                   Group
                 </label>
-                <input className={styles.inputField} type="text" onChange={event => setNewGroup(event.target.value)}/>
+                <input className={styles.inputField} type="text" value={newGroup} onChange={event => setNewGroup(event.target.value)}/>
               </div>
               <div className={styles.inputField}>
                 <label className={styles.inputFieldLabel}> 
                   Date
                 </label>
-                <input className={styles.inputField} type="text" onChange={event => setNewDate(event.target.value)} />
+                <input className={styles.inputField} type="text" value={newDate} onChange={event => setNewDate(event.target.value)} />
               </div>
               <div className={styles.inputField}>
                 <label className={styles.inputFieldLabel}> 
                   Description
                 </label>
-                <input className={styles.inputField} type="text" onChange={event => setNewDescription(event.target.value)} />
+                <input className={styles.inputField} type="text" value={newDescription} onChange={event => setNewDescription(event.target.value)} />
               </div>
               <div className={styles.inputField}>
                 <label className={styles.inputFieldLabel}> 
                   Type
                 </label>
-                <input className={styles.inputField} type="text" onChange={event => setNewType(event.target.value)} />
+                <input className={styles.inputField} type="text" value={newType} onChange={event => setNewType(event.target.value)} />
                </div>
                <div className={styles.button}>
                   <button onClick={ () => onAdd() }>Add</button>
